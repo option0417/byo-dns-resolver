be2971d88f5b4886d4d143f7646f0550861dee71
Wei De Su <weide_su@mitake.com.tw>
2025-08-25 18:31:18 +0800

    Move defination of message to separate file

--- main.go
+++ main.go
@@ -11,78 +11,16 @@ const (
 	DNS_Google   = "dns.google.com:53"
 	DNS_GoogleIP = "8.8.8.8:53"
 	DNS_MitakeIP = "10.3.1.254:53"
+	DefaultQuery = "00160100000100000000000003646e7306676f6f676c6503636f6d0000010001"
 )
 
-// Question section
-//
-//	                                1  1  1  1  1  1
-//	  0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-//	|                                               |
-//	/                     QNAME                     /
-//	/                                               /
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-//	|                     QTYPE                     |
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-//	|                     QCLASS                    |
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-type Question struct {
-	QName  string
-	QType  uint16
-	QClass uint16
-}
-
-// Resource record format
-//
-//	                                1  1  1  1  1  1
-//	  0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-//	|                                               |
-//	/                                               /
-//	/                      NAME                     /
-//	|                                               |
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-//	|                      TYPE                     |
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-//	|                     CLASS                     |
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-//	|                      TTL                      |
-//	|                                               |
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-//	|                   RDLENGTH                    |
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
-//	/                     RDATA                     /
-//	/                                               /
-//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-type ResourceRecoed struct {
-	Name     string
-	Type     uint16
-	Class    uint16
-	TTL      uint16
-	RDLength uint16
-	RData    uint16
-}
-
-// Message compression
-//    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-//    | 1  1|                OFFSET                   |
-//    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-
 func main() {
-	queryString := "00160100000100000000000003646e7306676f6f676c6503636f6d0000010001"
-
-	queryBytes, err := hex.DecodeString(queryString)
+	queryBytes, err := hex.DecodeString(DefaultQuery)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%s\n", queryString)
-	for _, v := range queryBytes {
-		fmt.Printf(" %d", v)
-	}
-	fmt.Println()
-
-	conn, err := net.Dial("udp", DNS_GoogleIP)
+	conn, err := net.Dial("udp", DNS_MitakeIP)
 	if err != nil {
 		panic(err)
 	}
--- messages.go
+++ messages.go
@@ -1,5 +1,20 @@
 package main
 
+import "strings"
+
+// DNS Message Format
+//  +---------------------+
+//  |        Header       |
+//  +---------------------+
+//  |       Question      | the question for the name server
+//  +---------------------+
+//  |        Answer       | RRs answering the question
+//  +---------------------+
+//  |      Authority      | RRs pointing toward an authority
+//  +---------------------+
+//  |      Additional     | RRs holding additional information
+//  +---------------------+
+
 // Header section
 //
 //	                              1  1  1  1  1  1
@@ -32,11 +47,174 @@ type Header struct {
 	ARCount uint16
 }
 
-func (this *Header) ToBytes() ([]byte, error) {
-	idBytes := make([]byte, 2)
+func (this *Header) fetchIDBytes() []byte {
+	if (this.Id == 0) || (this.Id > 65535) {
+		this.Id = uint16(1 + (randInt() % 65534))
+	}
 
-	idBytes[0] = byte((this.Id >> 8) & 0xFF)
-	idBytes[1] = byte(this.Id & 0xFF)
-
-	return idBytes, nil
+	return []byte{
+		byte((this.Id >> 8) & 0xFF),
+		byte(this.Id & 0xFF),
+	}
 }
+
+func (this *Header) fetchFlagBytes() []byte {
+	var flags uint16 = 0
+
+	if this.QR {
+		flags |= 1 << 15
+	}
+
+	flags |= (uint16(this.OPcode) & 0x0F) << 11
+
+	if this.AA {
+		flags |= 1 << 10
+	}
+
+	if this.TC {
+		flags |= 1 << 9
+	}
+
+	if this.RD {
+		flags |= 1 << 8
+	}
+
+	if this.RA {
+		flags |= 1 << 7
+	}
+
+	// Z is reserved and must be zero
+
+	flags |= uint16(this.RCode) & 0x0F
+
+	return []byte{
+		byte((flags >> 8) & 0xFF),
+		byte(flags & 0xFF),
+	}
+}
+
+func (this *Header) fetchQDCountBytes() []byte {
+	return []byte{
+		byte((this.QDCount >> 8) & 0xFF),
+		byte(this.QDCount & 0xFF),
+	}
+}
+
+func (this *Header) fetchANCountBytes() []byte {
+	return []byte{
+		byte((this.ANCount >> 8) & 0xFF),
+		byte(this.ANCount & 0xFF),
+	}
+}
+
+func (this *Header) fetchNSCountBytes() []byte {
+	return []byte{
+		byte((this.NSCount >> 8) & 0xFF),
+		byte(this.NSCount & 0xFF),
+	}
+}
+
+func (this *Header) fetchARCountBytes() []byte {
+	return []byte{
+		byte((this.ARCount >> 8) & 0xFF),
+		byte(this.ARCount & 0xFF),
+	}
+}
+
+func (this *Header) toBytes() []byte {
+	bytes := []byte{}
+	bytes = append(bytes, this.fetchIDBytes()...)
+	bytes = append(bytes, this.fetchFlagBytes()...)
+	bytes = append(bytes, this.fetchQDCountBytes()...)
+	bytes = append(bytes, this.fetchANCountBytes()...)
+	bytes = append(bytes, this.fetchNSCountBytes()...)
+	bytes = append(bytes, this.fetchARCountBytes()...)
+
+	return bytes
+}
+
+// Question section
+//
+//	                                1  1  1  1  1  1
+//	  0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//	|                                               |
+//	/                     QNAME                     /
+//	/                                               /
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//	|                     QTYPE                     |
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//	|                     QCLASS                    |
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+type Question struct {
+	QName  string
+	QType  uint16
+	QClass uint16
+}
+
+func (this *Question) fetchQNameBytes() []byte {
+	qNameParts := strings.Split(this.QName, ".")
+
+	var qNameBytes []byte
+
+	for _, part := range qNameParts {
+		length := len(part)
+		if length > 0 && length < 64 {
+			qNameBytes = append(qNameBytes, byte(length))
+			qNameBytes = append(qNameBytes, []byte(part)...)
+		}
+	}
+
+	qNameBytes = append(qNameBytes, 0) // End of QNAME
+	return qNameBytes
+}
+
+func (this *Question) fetchQTypeBytes() []byte {
+	return []byte{
+		byte((this.QType >> 8) & 0xFF),
+		byte(this.QType & 0xFF),
+	}
+}
+
+func (this *Question) fetchQClassBytes() []byte {
+	return []byte{
+		byte((this.QClass >> 8) & 0xFF),
+		byte(this.QClass & 0xFF),
+	}
+}
+
+// Resource record format
+//
+//	                                1  1  1  1  1  1
+//	  0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//	|                                               |
+//	/                                               /
+//	/                      NAME                     /
+//	|                                               |
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//	|                      TYPE                     |
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//	|                     CLASS                     |
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//	|                      TTL                      |
+//	|                                               |
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//	|                   RDLENGTH                    |
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
+//	/                     RDATA                     /
+//	/                                               /
+//	+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+type ResourceRecoed struct {
+	Name     string
+	Type     uint16
+	Class    uint16
+	TTL      uint16
+	RDLength uint16
+	RData    uint16
+}
+
+// Message compression
+//    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//    | 1  1|                OFFSET                   |
+//    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
